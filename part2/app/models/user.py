de3aa62a5d90d09624596667922f0759759d1b25
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-27 01:14:23 +0400

    user endpoints

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,4 +1,4 @@
-from .base_model import BaseModel
+from app.models.base_model import BaseModel
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
@@ -9,8 +10,18 @@ class HBnBFacade:
 
     # Placeholder method for creating a user
     def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
         # Logic will be implemented in later tasks
         pass
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
