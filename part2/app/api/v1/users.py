0bfaa57c6d68763e62b39e2eb53e2f8daf0fa579
meenakshee23 <meenaksheegopaul@gmail.com>
2026-03-01 22:55:49 +0400

    implementing

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
 
@@ -27,12 +29,11 @@ class UserList(Resource):
 
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
-
-@api.response(200, 'List of users retrieved successfully')
-def get(self):
-    """Retrieve list of all users"""
-    users = facade.get_all_users()
-    return [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email} for user in users], 200
+    @api.response(200, 'List of users retrieved successfully')
+    def get(self):
+        """Retrieve list of all users"""
+        users = facade.get_all_users()
+        return [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email} for user in users], 200
     
 @api.route('/<user_id>')
 class UserResource(Resource):
@@ -43,4 +44,21 @@ class UserResource(Resource):
         user = facade.get_user(user_id)
         if not user:
             return {'error': 'User not found'}, 404
+        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+    
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User successfully updated')
+    @api.response(404, 'User not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, user_id):
+        """Update a user by ID"""
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+
+        data = api.payload
+        user.first_name = data.get('first_name', user.first_name)
+        user.last_name = data.get('last_name', user.last_name)
+        user.email = data.get('email', user.email)
+
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -15,13 +15,15 @@ class HBnBFacade:
         return user
 
         # Logic will be implemented in later tasks
-        pass
 
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
+    
+    def get_all_users(self):
+        return self.user_repo.get_all() 
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
