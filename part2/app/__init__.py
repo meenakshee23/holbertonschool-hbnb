a30bf4e560a9e1e0681cf4bfc2967a84c3d83e2c
meenakshee23 <meenaksheegopaul@gmail.com>
2026-04-06 09:22:55 +0400

    update

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Register the users namespace
---
+++ part3/README.md
@@ -0,0 +1,3 @@
+### Introduction to Part 3: Enhanced Backend with Authentication and Database Integration
+
+Part 3:
